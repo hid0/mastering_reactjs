28cd3e493172d3cd1f194ff294b0e28ca21daef4
Faiz Hidayatulloh <faiz14hiedayatoelloh@gmail.com>
2022-03-07 15:27:59 +0700

    nothing else

--- src/next/TodoForm.js
+++ src/next/TodoForm.js
@@ -8,12 +8,15 @@ const TodoForm = () => {
         <input
           id="input"
           type="text"
-          className="w-full rounded-lg border focus:outline-none focus:ring-2 p-1 focus:ring-sky-400 focus:border-sky-600 focus:border-0 p-1"
+          className="w-full p-1 transition-all duration-150 border rounded-lg text-slate-800 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:border-sky-600"
         />
-        {/* <button className="text-white bg-sky-600 rounded-lg w-7 flex items-center mx-auto h-full p-1 hover:bg-sky-700 active:bg-sky-800">
+        <button
+          id="btn"
+          className="flex items-center h-full p-1 mx-auto text-white rounded-lg bg-sky-600 w-7 hover:bg-sky-700 active:bg-sky-800"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="w-6 h-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -25,7 +28,7 @@ const TodoForm = () => {
               d="M12 6v6m0 0v6m0-6h6m-6 0H6"
             />
           </svg>
-        </button> */}
+        </button>
       </div>
     </>
   );
--- src/next/TodoList.js
+++ src/next/TodoList.js
@@ -1,30 +1,36 @@
 import TodoForm from "./TodoForm";
 
-function reducer(todos, action) {
-  switch (action.type) {
-    case "ADD_TODO":
-      let nextId = todos.length ? todos[todos.length - 1].id + 1 : 1;
-      return [...todos, { id: nextId, completed: false, ...action.text }];
-    case "REMOVE_TODO":
-      return todos.filter((todo) => todo.id !== action.id);
-    case "UPDATE_TODO":
-      return todos.map((todo) =>
-        todo.id === action.todo.id ? action.todo : todo
-      );
-    case "TOGGLE_TODO":
-      return todos.map((todo) =>
-        todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
-      );
-    default:
-      return todos;
-  }
-}
+// function reducer(todos, action) {
+//   switch (action.type) {
+//     case "ADD_TODO":
+//       let nextId = todos.length ? todos[todos.length - 1].id + 1 : 1;
+//       return [...todos, { id: nextId, completed: false, ...action.text }];
+//     case "REMOVE_TODO":
+//       return todos.filter((todo) => todo.id !== action.id);
+//     case "UPDATE_TODO":
+//       return todos.map((todo) =>
+//         todo.id === action.todo.id ? action.todo : todo
+//       );
+//     case "TOGGLE_TODO":
+//       return todos.map((todo) =>
+//         todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+//       );
+//     default:
+//       return todos;
+//   }
+// }
 
 const Todo = () => {
+  // let input = document.getElementById("input");
+  // const btn = document.querySelector("#btn");
+
+  // btn.addEventListener("click", () => {
+  //   console.log(input.value);
+  // });
   return (
-    <div className="mx-auto bg-white rounded-md w-96 h-60 shadow p-4">
+    <div className="p-4 mx-auto bg-white rounded-md shadow w-96 h-60">
       <div className="">
-        <h1 className="text-3xl text-center font-semibold uppercase">Todo</h1>
+        <h1 className="text-3xl font-semibold text-center uppercase">Todo</h1>
         <TodoForm />
       </div>
     </div>
