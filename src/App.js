c18e0dfd952513d52b144019fd7c0750e68bccbb
Faiz Hidayatulloh <faizhid11@gmail.com>
2022-04-04 14:29:05 +0700

    routing in react router v6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Navigate, NavLink, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* navigation */}
+      <ul className="menu">
+        <li>
+          <NavLink to="/" activeClassName="active" exact>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" activeClassName="active">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/member" activeClassName="active">
+            Member
+          </NavLink>
+        </li>
+      </ul>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/member" element={<Navigate to="/" replace />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <h2>Ini Halaman Home</h2>;
