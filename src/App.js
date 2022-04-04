1ab89317399aa1305722e27c8fea753963015ffd
Faiz Hidayatulloh <faizhid11@gmail.com>
2022-04-04 13:05:58 +0700

    closing and next step

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
 
-const useToggle = function () {
-  const [value, toggle] = useState("OFF");
-
-  return [value, toggle];
-};
-
-const App = () => {
-  let [statusLampu, toggleLampu] = useToggle();
+function App() {
   return (
-    <>
-      <div>lampu saat ini : {statusLampu}</div> <br />
-      <button onClick={(e) => toggleLampu()}>Saklar</button>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="w-full h-screen font-sans bg-slate-50">
-      <App />
-    </div>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
